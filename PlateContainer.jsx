6e0003fc52bbf95ee9664783f74cc1c6256f8f73
nadanta <younger.james@gmail.com>
2025-07-25 15:29:28 +0100

    Add files via upload

---
+++ PlateContainer.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import FourOscPlate from './FourOscPlate'; // You must create/import this
+import NoisePlate from './NoisePlate';
+import NavMenu from './NavMenu';
+
+export default function PlateContainer() {
+  const [activePlate, setActivePlate] = useState('4osc');
+
+  return (
+    <div className="plate-wrapper">
+      <NavMenu active={activePlate} setActive={setActivePlate} />
+      {activePlate === '4osc' && <FourOscPlate />}
+      {activePlate === 'noise' && <NoisePlate />}
+    </div>
+  );
+}
